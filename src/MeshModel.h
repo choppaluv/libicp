567e97e540013cc63903d3127039ff29df3b3bb5
choppaluv <rlawldn1016@naver.com>
2019-12-14 18:43:04 +0900

    mesh model using ocf

--- src/MeshModel.h
+++ src/MeshModel.h
@@ -1,16 +1,17 @@
 #pragma once
 #include <vcg/complex/complex.h>
 
-class MyVertex;
-class MyFace;
+/* Opitonal Component Fast of vcg */
+class MyVertexOcf;
+class MyFaceOcf;
 class MyEdge;
 
-struct MyUsedTypes : public vcg::UsedTypes<
-	vcg::Use<MyVertex>   ::AsVertexType,
+struct MyUsedTypesOcf : public vcg::UsedTypes<
+	vcg::Use<MyVertexOcf>   ::AsVertexType,
 	vcg::Use<MyEdge>     ::AsEdgeType,
-	vcg::Use<MyFace>     ::AsFaceType> {};
+	vcg::Use<MyFaceOcf>     ::AsFaceType> {};
 
-class MyVertex : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::Curvatured, vcg::vertex::Color4b, vcg::vertex::BitFlags > {};
-class MyFace : public vcg::Face< MyUsedTypes, vcg::face::FFAdj, vcg::face::VertexRef, vcg::face::Color4b, vcg::face::BitFlags, vcg::face::Normal3f > {};
-class MyEdge : public vcg::Edge< MyUsedTypes> {};
-class MyMesh : public vcg::tri::TriMesh< std::vector<MyVertex>, std::vector<MyFace>, std::vector<MyEdge>  > {};
+class MyVertexOcf : public vcg::Vertex< MyUsedTypesOcf, vcg::vertex::InfoOcf, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::Curvatured, vcg::vertex::Color4b, vcg::vertex::BitFlags, vcg::vertex::MarkOcf > {};
+class MyFaceOcf : public vcg::Face< MyUsedTypesOcf, vcg::face::InfoOcf, vcg::face::FFAdjOcf, vcg::face::VertexRef, vcg::face::Color4b, vcg::face::BitFlags, vcg::face::Normal3f, vcg::face::MarkOcf > {};
+class MyEdge : public vcg::Edge< MyUsedTypesOcf> {};
+class MyMeshOcf : public vcg::tri::TriMesh< vcg::vertex::vector_ocf<MyVertexOcf>, vcg::face::vector_ocf<MyFaceOcf> > {};
